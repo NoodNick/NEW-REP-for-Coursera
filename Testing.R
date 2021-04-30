a51b9f77a5ae1509de7ae8898bab3f8cc1fd631c
Al Msvi <82646229+NoodNick@users.noreply.github.com>
2021-04-30 11:49:42 +0200

    Update Testing.R
    comment jadid akhari ke mikhastamo daram check mikonam

--- Testing.R
+++ Testing.R
@@ -1,3 +1,6 @@
 print("This file was created within RStudio")
 print("And now it lives on GitHub")
 pashmam
+inja check mikonam , save mikonam , baad bebinam mishe pull kard roo desktop (in avalish roo github website hast , injoori maloom mishe balad shodam ta hodoodi)
+berim jelo bebinim chejoorie 
+ghesmate comment ham ye chizi mizaram bebinamesh 
